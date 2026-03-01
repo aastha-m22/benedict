0e1a4a386c8d2d7ab65eae0c3695502c2f611dd9
Michael Karotsieris <michael.karotsieris@gmail.com>
2026-03-01 23:58:59 +0000

    .

---
+++ src/benedict/commands/__init__.py
@@ -0,0 +1,42 @@
+"""Command Classification System
+
+Provides intent-based command routing for Benedict.
+Commands are declaratively defined and automatically classified from user input.
+"""
+
+from .command_classifier import CommandClassifier, CommandType, CommandIntent
+from .command_definitions import COMMAND_DEFINITIONS
+from .tool_framework import Tool, ToolResult, ToolRegistry
+from .tool_registry_factory import create_tool_registry, create_tool_registry_from_method_data
+from .method_tools import (
+    UpdatePCTool,
+    UpdateConcernTool,
+    GetMethodStateTool,
+    UpdateSequencePhaseTool,
+)
+from .metadata_tools import (
+    GetFileMetadataTool,
+    ListKeyFilesTool,
+    GetRepositorySummaryTool,
+)
+from .llm_classifier import LLMCommandClassifier
+
+__all__ = [
+    "CommandClassifier",
+    "CommandType",
+    "CommandIntent",
+    "COMMAND_DEFINITIONS",
+    "Tool",
+    "ToolResult",
+    "ToolRegistry",
+    "create_tool_registry",
+    "create_tool_registry_from_method_data",
+    "UpdatePCTool",
+    "UpdateConcernTool",
+    "GetMethodStateTool",
+    "UpdateSequencePhaseTool",
+    "GetFileMetadataTool",
+    "ListKeyFilesTool",
+    "GetRepositorySummaryTool",
+    "LLMCommandClassifier",
+]
